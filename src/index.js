780f264d7810747f93fef416500952adff83b555
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-09 22:06:46 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
-    if(array === undefined) || array == 0
+    if(array === undefined || array == 0)
         return 0;
     let Avg = 0;
     for(let i = 0;i<array.length;i++) {

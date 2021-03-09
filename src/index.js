dafbdb8084538dab9f02d0bcec95a32dd42f4d30
valdik-g <77793654+valdik-g@users.noreply.github.com>
2021-03-09 17:01:50 +0300

    feat: change index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined)
+        return 0;
+    let ArMin = array[0];
+    for(let i = 1;i<array.length;i++) {
+        if(array[i] <ArMin)
+            ArMin = array[i];
+    }
+    return ArMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined)
+        return 0;
+    let ArMax = array[0];
+    for(let i = 1;i<array.length;i++) {
+        if(array[i] >ArMax)
+            ArMax = array[i];
+    }
+    return ArMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined)
+        return 0;
+    let Avg = 0;
+    for(let i = 0;i<array.length;i++) {
+        Avg+=array[i];
+    }
+    Avg = Avg/array.length;
+    return Avg;
 }
